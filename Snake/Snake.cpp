c338cba0f421160c2bd99f68c20b0dad4c5501a4
Maksymilian Wrzesiński <andre.pal.wie.22@gmail.com>
2023-05-28 11:14:53 +0200

    Added moving tail and warp on wall hit

--- Snake/Snake.cpp
+++ Snake/Snake.cpp
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <conio.h>
+#include <Windows.h>
 
 using namespace std;
 
@@ -12,6 +13,10 @@ const int width = 20;
 const int height = 20;
 
 int x, y, fruitX, fruitY, score;
+
+int tailX[100], tailY[100];
+
+int nTail; //tail length
 
 enum eDirection{ STOP = 0, LEFT, RIGHT, UP, DOWN};
 eDirection dir;
@@ -46,8 +51,18 @@ void Draw() {
                 cout << "O";
             else if (i == fruitY && j == fruitX)
                 cout << "F";
-            else
-                cout << " ";
+            else {
+                bool print = false;
+                for (int z = 0; z < nTail; z++) {
+                    if (tailX[z] == j && tailY[z] == i) {
+                        cout << "o";
+                        print = true;
+                    }
+                }
+                if (!print)
+                    cout << " ";
+            }
+
 
             if (j == width - 1)
                 cout << "#";
@@ -64,6 +79,21 @@ void Draw() {
 }
 
 void Logic() {
+
+    int prevX = tailX[0]; //previous tail segment X coordinate
+    int prevY = tailY[0]; //previous tail segment Y coordinate
+    int prev2X, prev2Y;
+    tailX[0] = x;
+    tailY[0] = y;
+
+    for (int i = 1; i < nTail; i++) {
+        prev2X = tailX[i];
+        prev2Y = tailY[i];
+        tailX[i] = prevX;
+        tailY[i] = prevY;
+        prevX = prev2X;
+        prevY = prev2Y; 
+    }
 
     switch (dir) {
     case DOWN:
@@ -82,10 +112,22 @@ void Logic() {
         break;
     }
 
-    if (x > width || x< 0 || y>height || y < 0)
-        gameover = true;
+   // if (x > width || x< 0 || y>height || y < 0)//death on wall hit
+   //     gameover = true;
+    if (x >= width) //
+        x = 0;// appear from other side on wall hit
+    else if (x < 0)//
+        x = width - 1;//
+    if (y >= height) //
+        y = 0;// appear from other side on wall hit
+    else if (y < 0)//
+        y = height - 1;//
+    for (int i = 0; i < nTail; i++)
+        if (tailX[i] == x && tailY[i] == y)
+            gameover = true;
     if (x == fruitX && y == fruitY) {
         score+=10;
+        nTail++;
         fruitX = rand() % width;
         fruitY = rand() % height;
     }
@@ -127,6 +169,7 @@ int main()
         Draw();
         Input();
         Logic();
+        Sleep(150);
     }
 
 
